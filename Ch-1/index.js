cf06c20c1d5795782cb09657cd894313b779cf41
Himanshi <himanshiagarwal001@gmail.com>
2026-02-13 23:53:05 +0530

    changed file

--- Ch-1/index.js
+++ Ch-1/index.js
@@ -5,7 +5,7 @@ if (status === "new") {
  if (scared) {
  console.log("Don't worry you will be fine!");
  } else {
- console.log("You're brave! You are going to do great!");
+ console.log(" You are going to do great!");
  }
 } else {
  console.log("Welcome back, I knew you'd like it!");
